71fa37192cf8a261d69abd4b7df81eea7ed12fff
sean-dickinson <90267290+sean-dickinson@users.noreply.github.com>
2024-12-03 19:36:02 -0500

    feat: part 1

--- 03.rb
+++ 03.rb
@@ -1,7 +1,28 @@
 module Day03
+  Multiplier = Data.define(:x, :y) do
+    def result
+      x * y
+    end
+  end
+
+  class MultiplierParser
+    # @param input [String]
+    def initialize(input)
+      @input = input
+    end
+
+    # @return [Array<Multiplier>]
+    def parse
+      result = []
+      @input.scan(/mul\((\d{1,3}),(\d{1,3})\)/) do |x, y|
+        result << Multiplier.new(x.to_i, y.to_i)
+      end
+      result
+    end
+  end
   class << self
     def part_one(input)
-      raise NotImplementedError
+      MultiplierParser.new(input.join).parse.sum(&:result)
     end
 
     def part_two(input)
