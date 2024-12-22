15694c9aad1ae90e6bdf62ef90cb0402a67b13a3
sean-dickinson <90267290+sean-dickinson@users.noreply.github.com>
2024-12-22 15:49:52 -0500

    feat: part 1

--- 05.rb
+++ 05.rb
@@ -1,7 +1,79 @@
 module Day05
+  class Update
+    attr_reader :pages
+    # @param string [String]
+    def initialize(string)
+      @pages = string.split(",").map(&:to_i)
+    end
+
+    def middle
+      pages[middle_index]
+    end
+
+    private
+
+    def middle_index
+      (@pages.size / 2).ceil
+    end
+  end
+
+  class Rule
+    # @param string [String]
+    def initialize(string)
+      @before, @after = string.split("|").map(&:to_i)
+    end
+
+    # @param update [Update]
+    # @return [Boolean]
+    def valid?(update)
+      before_index = update.pages.find_index(@before)
+      after_index = update.pages.find_index(@after)
+      return true if before_index.nil? || after_index.nil?
+
+      before_index < after_index
+    end
+  end
+
+  class Validator
+    class << self
+      def from_input(lines)
+        rules = []
+        updates = []
+        is_rule = true
+        lines.each do |line|
+          if line.empty?
+            is_rule = false
+            next
+          end
+          if is_rule
+            rules << Rule.new(line)
+          else
+            updates << Update.new(line)
+          end
+        end
+        new(rules, updates)
+      end
+    end
+
+    attr_reader :rules, :updates
+    # @param rules [Array<Rule>]
+    # @param updates [Array<Update>]
+
+    def initialize(rules, updates)
+      @rules = rules
+      @updates = updates
+    end
+
+    def valid_updates
+      @updates.select do |update|
+        @rules.all? { |rule| rule.valid?(update) }
+      end
+    end
+  end
   class << self
     def part_one(input)
-      raise NotImplementedError
+      validator = Validator.from_input(input)
+      validator.valid_updates.sum(&:middle)
     end
 
     def part_two(input)
--- spec/05_spec.rb
+++ spec/05_spec.rb
@@ -1,10 +1,116 @@
 require "./05"
 RSpec.describe Day05 do
+  describe "Rule" do
+    it "is created from a string" do
+      rule = Day05::Rule.new("53|13")
+      expect(rule).to be_a(Day05::Rule)
+    end
+
+    describe "#valid?" do
+      it "is true if none of the update page numbers apply" do
+        rule = Day05::Rule.new("53|13")
+        update = Day05::Update.new("1,2,3")
+        expect(rule.valid?(update)).to eq true
+      end
+
+      it "is true if the page numbers are in the correct order" do
+        rule = Day05::Rule.new("53|13")
+        update = Day05::Update.new("53,13,1")
+        expect(rule.valid?(update)).to eq true
+      end
+
+      it "is true when the page numbers are in the correct order even if other numbers are in between them" do
+        rule = Day05::Rule.new("53|13")
+        update = Day05::Update.new("53,1,13")
+        expect(rule.valid?(update)).to eq true
+      end
+
+      it "is false if the page numbers are in the wrong order" do
+        rule = Day05::Rule.new("53|13")
+        update = Day05::Update.new("13,53,1")
+        expect(rule.valid?(update)).to eq false
+      end
+    end
+  end
+
+  describe "Update" do
+    it "is created from a string" do
+      update = Day05::Update.new("75,47,61,53,29")
+      expect(update).to be_a(Day05::Update)
+    end
+
+    it "responds to middle with the middle number" do
+      update = Day05::Update.new("75,47,61,53,29")
+      expect(update.middle).to eq 61
+    end
+  end
+
+  describe "Validator" do
+    it "is created from an array of rules and an array of updates" do
+      rules = [
+        Day05::Rule.new("75|47"),
+        Day05::Rule.new("75|61"),
+        Day05::Rule.new("75|53"),
+        Day05::Rule.new("75|29")
+      ]
+      updates = [
+        Day05::Update.new("75,47,61,53,29")
+      ]
+      validator = Day05::Validator.new(rules, updates)
+      expect(validator).to be_a(Day05::Validator)
+    end
+
+    describe "#valid_updates" do
+      it "returns an array of updates that are valid" do
+        rules = [
+          Day05::Rule.new("75|47"),
+          Day05::Rule.new("75|61"),
+          Day05::Rule.new("75|53"),
+          Day05::Rule.new("75|29"),
+          Day05::Rule.new("97|75") # makes 2nd update invalid
+        ]
+        updates = [
+          Day05::Update.new("75,47,61,53,29"),
+          Day05::Update.new("75,97,47,61,53")
+
+        ]
+        validator = Day05::Validator.new(rules, updates)
+
+        valid_updates = validator.valid_updates
+        expect(valid_updates.size).to eq 1
+        expect(valid_updates.first.pages).to eq [75, 47, 61, 53, 29]
+      end
+    end
+
+    describe "self.from_input" do
+      it "creates a validator from an array of strings" do
+        input = [
+          "75|47",
+          "75|61",
+          "75|53",
+          "75|29",
+          "97|75",
+          "",
+          "75,47,61,53,29",
+          "75,97,47,61,53"
+        ]
+        validator = Day05::Validator.from_input(input)
+        expect(validator).to be_a(Day05::Validator)
+      end
+
+      it "can correctly parse the input file" do
+        input = File.readlines("spec/test_inputs/05.txt", chomp: true)
+        validator = Day05::Validator.from_input(input)
+        expect(validator.rules.size).to eq 21
+        expect(validator.updates.size).to eq 6
+      end
+    end
+  end
+
   context "part 1" do
     it "returns the correct answer for the example input" do
-      pending
       input = File.readlines("spec/test_inputs/05.txt", chomp: true)
-      expect(Day05.part_one(input)).to eq 0 # TODO: replace with correct answer
+      expect(Day05.part_one(input)).to eq 143
     end
   end
 
