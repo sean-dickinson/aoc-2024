974ef53fd44ae94c9a40ec6a9484b40399e17ec1
sean-dickinson <90267290+sean-dickinson@users.noreply.github.com>
2024-12-23 14:03:10 -0500

    feat: part 1

--- 06.rb
+++ 06.rb
@@ -1,10 +1,144 @@
+# frozen_string_literal: true
+
 module Day06
+  HEADINGS = %i[north east south west].freeze
+
+  Position = Data.define(:x, :y) do
+    def translate(direction)
+      case direction
+      when :north
+        Position.new(x, y - 1)
+      when :south
+        Position.new(x, y + 1)
+      when :east
+        Position.new(x + 1, y)
+      when :west
+        Position.new(x - 1, y)
+      else
+        raise ArgumentError, "Invalid direction: #{direction}"
+      end
+    end
+  end
+
+  class Guard
+    attr_reader :position, :heading
+
+    def initialize(position, heading)
+      @position = position
+      @heading = heading
+      @visited_positions = Set.new
+      record_position!
+    end
+
+    def next_position
+      @position.translate(@heading)
+    end
+
+    def forward
+      @position = next_position
+      record_position!
+    end
+
+    def turn
+      current_index = HEADINGS.index(@heading)
+      new_index = (current_index + 1) % HEADINGS.length
+      @heading = HEADINGS[new_index]
+    end
+
+    def visited_positions
+      @visited_positions.to_a
+    end
+
+    private
+
+    def record_position!
+      @visited_positions << @position
+    end
+  end
+
+  MapBounds = Data.define(:rows, :columns) do
+    def out_of_bounds?(position)
+      position.x.negative? || position.x >= columns || position.y.negative? || position.y >= rows
+    end
+  end
+
+  class Map
+    attr_reader :bounds, :obstacles, :guard
+
+    def initialize(bounds:, obstacles:, guard:)
+      @bounds = bounds
+      @obstacles = obstacles
+      @guard = guard
+    end
+
+    def plot
+      until @bounds.out_of_bounds?(@guard.next_position)
+        if @obstacles.include?(@guard.next_position)
+          @guard.turn
+        else
+          @guard.forward
+        end
+      end
+    end
+  end
+
+  class MapFactory
+    def initialize(input)
+      @input = input
+    end
+
+    def build
+      Map.new(bounds:, obstacles:, guard:)
+    end
+
+    private
+
+    def bounds
+      MapBounds.new(@input.size, @input.first.size)
+    end
+
+    def obstacles
+      input_value_and_positions do |cell, position|
+        position if cell == "#"
+      end.compact
+    end
+
+    def guard
+      input_value_and_positions do |cell, position|
+        return Guard.new(position, heading_mappings[cell]) if heading_mappings.key?(cell)
+      end
+    end
+
+    def input_value_and_positions
+      @input.each_with_index.flat_map do |row, y|
+        row.chars.each_with_index.map do |cell, x|
+          yield(cell, Position.new(x, y))
+        end
+      end
+    end
+
+    def heading_mappings
+      {
+        "^" => :north,
+        ">" => :east,
+        "v" => :south,
+        "<" => :west
+      }
+    end
+  end
+
   class << self
     def part_one(input)
-      raise NotImplementedError
+      map = MapFactory.new(input).build
+      map.plot
+      map.guard.visited_positions.size
     end
 
     def part_two(input)
+      # get options via the positions from part 1 - starting position
+      # try adding an obstacle for each one and run the plot
+      # if the guard leaves the bounds, we reject it
+      # TODO: figure out how to end early for a loop so we don't end up in a stack overflow
       raise NotImplementedError
     end
   end
--- spec/06_spec.rb
+++ spec/06_spec.rb
@@ -1,10 +1,201 @@
 require "./06"
 RSpec.describe Day06 do
+  describe "Position" do
+    it "is an object with an x and y coordinate" do
+      pos = Day06::Position.new(1, 2)
+      expect(pos).to be_a Day06::Position
+      expect(pos.x).to eq 1
+      expect(pos.y).to eq 2
+    end
+
+    it "behaves like a value object" do
+      pos1 = Day06::Position.new(1, 2)
+      pos2 = Day06::Position.new(1, 2)
+      expect(pos1).to eq pos2
+    end
+
+    describe "#translate" do
+      it "returns a new position with the x or y coordinate adjusted by 1" do
+        pos = Day06::Position.new(1, 2)
+        expect(pos.translate(:north)).to eq Day06::Position.new(1, 1)
+        expect(pos.translate(:south)).to eq Day06::Position.new(1, 3)
+        expect(pos.translate(:east)).to eq Day06::Position.new(2, 2)
+        expect(pos.translate(:west)).to eq Day06::Position.new(0, 2)
+      end
+    end
+  end
+
+  describe "Guard" do
+    it "is initialized with a position and a heading" do
+      pos = Day06::Position.new(1, 2)
+      guard = Day06::Guard.new(pos, :north)
+      expect(guard).to be_a Day06::Guard
+      expect(guard.position).to eq pos
+      expect(guard.heading).to eq :north
+    end
+
+    context "#forward" do
+      it "decrements the y position if facing north" do
+        pos = Day06::Position.new(1, 2)
+        guard = Day06::Guard.new(pos, :north)
+        guard.forward
+        expect(guard.position).to eq Day06::Position.new(1, 1)
+      end
+
+      it "increments the y position if facing south" do
+        pos = Day06::Position.new(1, 2)
+        guard = Day06::Guard.new(pos, :south)
+        guard.forward
+        expect(guard.position).to eq Day06::Position.new(1, 3)
+      end
+
+      it "increments the x position if facing east" do
+        pos = Day06::Position.new(1, 2)
+        guard = Day06::Guard.new(pos, :east)
+        guard.forward
+        expect(guard.position).to eq Day06::Position.new(2, 2)
+      end
+
+      it "decrements the x position if facing west" do
+        pos = Day06::Position.new(1, 2)
+        guard = Day06::Guard.new(pos, :west)
+        guard.forward
+        expect(guard.position).to eq Day06::Position.new(0, 2)
+      end
+    end
+
+    context "#next_position" do
+      it "returns the position the guard would move to if it moved forward, but does not move the guard" do
+        pos = Day06::Position.new(1, 2)
+        guard = Day06::Guard.new(pos, :north)
+        expect(guard.next_position).to eq Day06::Position.new(1, 1)
+      end
+    end
+
+    context "#turn" do
+      it "rotates the guard 90 degrees to the right, by updating the heading" do
+        pos = Day06::Position.new(1, 2)
+        guard = Day06::Guard.new(pos, :north)
+
+        guard.turn
+        expect(guard.heading).to eq :east
+        guard.turn
+        expect(guard.heading).to eq :south
+        guard.turn
+        expect(guard.heading).to eq :west
+        guard.turn
+        expect(guard.heading).to eq :north
+      end
+    end
+
+    context "#visited_positions" do
+      it "returns a list of all the positions the guard has visited" do
+        pos = Day06::Position.new(1, 1)
+        guard = Day06::Guard.new(pos, :north)
+
+        expect(guard.visited_positions).to eq [pos]
+
+        guard.forward
+        expect(guard.visited_positions).to eq [pos, Day06::Position.new(1, 0)]
+
+        guard.turn
+        guard.forward
+        expect(guard.visited_positions).to eq [pos, Day06::Position.new(1, 0), Day06::Position.new(2, 0)]
+      end
+    end
+  end
+
+  describe "MapBounds" do
+    it "is initialized with a number of rows and columns" do
+      bounds = Day06::MapBounds.new(3, 4)
+      expect(bounds).to be_a Day06::MapBounds
+      expect(bounds.rows).to eq 3
+      expect(bounds.columns).to eq 4
+    end
+
+    context "#out_of_bounds?" do
+      it "returns true if the both the x and y are outside the bounds" do
+        bounds = Day06::MapBounds.new(3, 3)
+        expect(bounds.out_of_bounds?(Day06::Position.new(3, 3))).to be true
+        expect(bounds.out_of_bounds?(Day06::Position.new(4, 4))).to be true
+        expect(bounds.out_of_bounds?(Day06::Position.new(-1, -1))).to be true
+      end
+
+      it "returns true if the x is outside the bounds" do
+        bounds = Day06::MapBounds.new(3, 3)
+        expect(bounds.out_of_bounds?(Day06::Position.new(3, 2))).to be true
+        expect(bounds.out_of_bounds?(Day06::Position.new(4, 2))).to be true
+        expect(bounds.out_of_bounds?(Day06::Position.new(-1, 2))).to be true
+      end
+
+      it "returns true if the y is outside the bounds" do
+        bounds = Day06::MapBounds.new(3, 3)
+        expect(bounds.out_of_bounds?(Day06::Position.new(2, 3))).to be true
+        expect(bounds.out_of_bounds?(Day06::Position.new(2, 4))).to be true
+        expect(bounds.out_of_bounds?(Day06::Position.new(2, -1))).to be true
+      end
+
+      it "returns false if the position is within the bounds" do
+        bounds = Day06::MapBounds.new(3, 3)
+        expect(bounds.out_of_bounds?(Day06::Position.new(2, 2))).to be false
+        expect(bounds.out_of_bounds?(Day06::Position.new(1, 1))).to be false
+        expect(bounds.out_of_bounds?(Day06::Position.new(0, 0))).to be false
+        expect(bounds.out_of_bounds?(Day06::Position.new(2, 0))).to be false
+        expect(bounds.out_of_bounds?(Day06::Position.new(0, 2))).to be false
+      end
+    end
+  end
+
+  describe "Map" do
+    it "initializes with a set of bounds,  obstacles and a guard" do
+      bounds = Day06::MapBounds.new(3, 3)
+      obstacles = [Day06::Position.new(0, 1)]
+      guard = Day06::Guard.new(Day06::Position.new(2, 2), :north)
+      map = Day06::Map.new(bounds:, obstacles:, guard:)
+
+      expect(map).to be_a Day06::Map
+      expect(map.bounds).to eq bounds
+      expect(map.obstacles).to eq obstacles
+      expect(map.guard).to eq guard
+    end
+
+    describe "#plot" do
+      it "moves the guard until it is off the map" do
+        map = Day06::MapFactory.new([
+          "..#",
+          "#..",
+          "..^"
+        ]).build
+        map.plot
+        expect(map.guard.position).to eq Day06::Position.new(2, 1)
+        expect(map.guard.heading).to eq :east
+        expect(map.guard.visited_positions).to eq [
+          Day06::Position.new(2, 2),
+          Day06::Position.new(2, 1)
+        ]
+      end
+    end
+  end
+
+  describe "MapFactory" do
+    it "creates a map from an array of strings" do
+      map_factory = Day06::MapFactory.new([
+        "..#",
+        "#..",
+        "..^"
+      ])
+      map = map_factory.build
+
+      expect(map).to be_a Day06::Map
+      expect(map.obstacles).to eq [Day06::Position.new(2, 0), Day06::Position.new(0, 1)]
+      expect(map.guard.position).to eq Day06::Position.new(2, 2)
+    end
+  end
+
   context "part 1" do
     it "returns the correct answer for the example input" do
-      pending
       input = File.readlines("spec/test_inputs/06.txt", chomp: true)
-      expect(Day06.part_one(input)).to eq 0 # TODO: replace with correct answer
+      expect(Day06.part_one(input)).to eq 41
     end
   end
 
