ac5b0b0bb39c317b61d0f6ae71026623d2e2380a
sean-dickinson <90267290+sean-dickinson@users.noreply.github.com>
2025-01-05 14:33:45 -0500

    chore: scaffold

---
+++ 08.rb
@@ -0,0 +1,11 @@
+module Day08
+  class << self
+    def part_one(input)
+      raise NotImplementedError
+    end
+
+    def part_two(input)
+      raise NotImplementedError
+    end
+  end
+end
---
+++ spec/08_spec.rb
@@ -0,0 +1,18 @@
+require "./08"
+RSpec.describe Day08 do
+  context "part 1" do
+    it "returns the correct answer for the example input" do
+      pending
+      input = File.readlines("spec/test_inputs/08.txt", chomp: true)
+      expect(Day08.part_one(input)).to eq 0 # TODO: replace with correct answer
+    end
+  end
+
+  context "part 2" do
+    it "returns the correct answer for the example input" do
+      pending
+      input = File.readlines("spec/test_inputs/08.txt", chomp: true)
+      expect(Day08.part_two(input)).to eq 0 # TODO: replace with correct answer
+    end
+  end
+end
