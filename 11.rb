cae698841f31cb5a6c064aa147d2521eddb3355e
sean-dickinson <90267290+sean-dickinson@users.noreply.github.com>
2025-01-26 15:46:45 -0500

    feat: cache

--- 11.rb
+++ 11.rb
@@ -1,44 +1,91 @@
 require "forwardable"
 module Day11
   StoneCollection = Data.define(:stones) do
+    class << self
+      def from_numbers(numbers)
+        new(numbers.map { |number| Stone.new(number: number) })
+      end
+    end
+
     extend Forwardable
 
     def_delegators :stones, :size
 
     def +(other)
-      with(stones: stones + other.stones)
+      if other.is_a? Stone
+        with(stones: stones + [other])
+      elsif other.is_a? StoneCollection
+        with(stones: stones + other.stones)
+      else
+        raise ArgumentError, "Can only add a Stone or a StoneCollection"
+      end
     end
 
-    def blink
-      return blink_one if one?
-
-      stones.each_slice(size / 2).map do |some_stones|
-        with(stones: some_stones).blink
+    def blink(iterations = 1)
+      stones.map do |stone|
+        stone.blink(iterations)
       end.inject(:+)
+    end
+  end
+
+  Stone = Data.define(:number) do
+    @cache = {}
+
+    class << self
+      def get_closet_iteration_for(stone, iteration)
+        @cache[stone] ||= [stone.blink]
+        iteration_index = iteration - 1
+        value = @cache[stone][iteration_index]
+        return [iteration, value] unless value.nil?
+
+        @cache[stone] << @cache[stone].last.blink
+        highest_iteration_for(stone)
+      end
+
+      private
+
+      def highest_iteration_for(stone)
+        [
+          @cache[stone].size,
+          @cache[stone].last
+        ]
+      end
+    end
+
+    def +(other)
+      if other.is_a? Stone
+        StoneCollection.from_numbers([number, other.number])
+      elsif other.is_a? StoneCollection
+        StoneCollection.from_numbers([number]) + other
+      else
+        raise ArgumentError, "Can only add a Stone or a StoneCollection"
+      end
+    end
+
+    def blink(iterations = 1)
+      return base_blink if iterations == 1
+      iteration, value = get_closest_iteration_to(iterations)
+      if iteration == iterations
+        value
+      else
+        value.blink(iterations - iteration)
+      end
     end
 
     private
 
-    def from(s)
-      with(stones: s)
+    def get_closest_iteration_to(iteration)
+      self.class.get_closet_iteration_for(self, iteration)
     end
 
-    def blink_one
-      return with(stones: [1]) if number.zero?
+    def base_blink
+      return with(number: 1) if number.zero?
 
       if digit_count.even?
-        with(stones: split_number)
+        StoneCollection.from_numbers(split_number)
       else
-        with(stones: [number * 2024])
+        with(number: number * 2024)
       end
-    end
-
-    def number
-      stones.first
-    end
-
-    def one?
-      size == 1
     end
 
     def digit_count
@@ -56,21 +103,19 @@ module Day11
     end
 
     def create!
-      StoneCollection.new(@input.split.map(&:to_i))
+      StoneCollection.from_numbers(@input.split.map(&:to_i))
     end
   end
 
   class << self
     def part_one(input)
       stones = StoneFactory.new(input.first).create!
-      25.times do
-        stones = stones.blink
-      end
-      stones.size
+      stones.blink(25).size
     end
 
     def part_two(input)
-      raise NotImplementedError
+      stones = StoneFactory.new(input.first).create!
+      stones.blink(35).size
     end
   end
 end
--- spec/11_spec.rb
+++ spec/11_spec.rb
@@ -1,4 +1,5 @@
 require "./11"
+require "benchmark"
 RSpec.describe Day11 do
   describe Day11::StoneCollection do
     describe "#size" do
@@ -18,33 +19,45 @@ RSpec.describe Day11 do
 
     describe "#blink" do
       context "single stone" do
-        it "is returns by a stone with number 1 if it has a number 0" do
-          stone = Day11::StoneCollection.new([0])
-          expect(stone.blink).to eq Day11::StoneCollection.new([1])
-        end
-
-        context "even digits" do
-          it "becomes 2 stones with the digits split between them" do
-            stone = Day11::StoneCollection.new([12])
-            expect(stone.blink).to eq Day11::StoneCollection.new([1, 2])
+        context "single blink" do
+          it "is returns by a stone with number 1 if it has a number 0" do
+            stone = Day11::Stone.new(0)
+            expect(stone.blink).to eq Day11::Stone.new(1)
           end
 
-          it "ignores leading zeros" do
-            stone = Day11::StoneCollection.new([1000])
-            expect(stone.blink).to eq Day11::StoneCollection.new([10, 0])
+          context "even digits" do
+            it "becomes 2 stones with the digits split between them" do
+              stone = Day11::Stone.new(12)
+              expect(stone.blink).to eq Day11::StoneCollection.from_numbers([1, 2])
+            end
+
+            it "ignores leading zeros" do
+              stone = Day11::Stone.new(1000)
+              expect(stone.blink).to eq Day11::StoneCollection.from_numbers([10, 0])
+            end
+          end
+
+          it "is replaced by a stone with its number multiplied by 2024 if neither rule applies" do
+            stone = Day11::Stone.new(3)
+            expect(stone.blink).to eq Day11::Stone.new(3 * 2024)
           end
         end
-
-        it "is replaced by a stone with its number multiplied by 2024 if neither rule applies" do
-          stone = Day11::StoneCollection.new([3])
-          expect(stone.blink).to eq Day11::StoneCollection.new([3 * 2024])
+        context "multiple blinks" do
+          it "returns the correct stone for the second blink" do
+            stone = Day11::Stone.new(0)
+            expect(stone.blink(2)).to eq Day11::Stone.new(2024)
+          end
+          it "returns the correct stone for the third blink" do
+            stone = Day11::Stone.new(0)
+            expect(stone.blink(3)).to eq Day11::StoneCollection.from_numbers([20, 24])
+          end
         end
       end
 
       context "multiple stones" do
         it "blinks each stone" do
-          stones = Day11::StoneCollection.new([1, 2])
-          expect(stones.blink).to eq Day11::StoneCollection.new([1]).blink + Day11::StoneCollection.new([2]).blink
+          stones = Day11::StoneCollection.from_numbers([0, 1])
+          expect(stones.blink).to eq Day11::StoneCollection.from_numbers([1, 2024])
         end
       end
     end
@@ -54,26 +67,26 @@ RSpec.describe Day11 do
     it "parses a list of stones from an string" do
       stone_factory = Day11::StoneFactory.new("0 1 10 99 999")
       stones = stone_factory.create!
-      expect(stones).to eq Day11::StoneCollection.new([0, 1, 10, 99, 999])
+      expect(stones).to eq Day11::StoneCollection.from_numbers([0, 1, 10, 99, 999])
     end
   end
 
   context "examples" do
     it "returns the correct answer for the first blink" do
-      stones = Day11::StoneCollection.new([125, 17])
-      expect(stones.blink).to eq Day11::StoneCollection.new([253000, 1, 7])
+      stones = Day11::StoneCollection.from_numbers([125, 17])
+      expect(stones.blink).to eq Day11::StoneCollection.from_numbers([253000, 1, 7])
     end
 
     it "returns the correct answer for the second blink" do
-      stones = Day11::StoneCollection.new([253000, 1, 7])
+      stones = Day11::StoneCollection.from_numbers([125, 17])
 
-      expect(stones.blink).to eq Day11::StoneCollection.new([253, 0, 2024, 14168])
+      expect(stones.blink(2)).to eq Day11::StoneCollection.from_numbers([253, 0, 2024, 14168])
     end
 
     it "returns the correct answer for the 3rd blink" do
-      stones = Day11::StoneCollection.new([253, 0, 2024, 14168])
+      stones = Day11::StoneCollection.from_numbers([125, 17])
 
-      expect(stones.blink).to eq Day11::StoneCollection.new([512072, 1, 20, 24, 28676032])
+      expect(stones.blink(3)).to eq Day11::StoneCollection.from_numbers([512072, 1, 20, 24, 28676032])
     end
   end
 
@@ -81,14 +94,6 @@ RSpec.describe Day11 do
     it "returns the correct answer for the example input" do
       input = File.readlines("spec/test_inputs/11.txt", chomp: true)
       expect(Day11.part_one(input)).to eq 55312
-    end
-  end
-
-  context "part 2" do
-    it "returns the correct answer for the example input" do
-      pending
-      input = File.readlines("spec/test_inputs/11.txt", chomp: true)
-      expect(Day11.part_two(input)).to eq 0 # TODO: replace with correct answer
     end
   end
 end
